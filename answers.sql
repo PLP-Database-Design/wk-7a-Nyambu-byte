3b6fa24463405bed34607eb3a38e580d033fb127
Nyambu-byte <nyamburachereen@gmail.com>
2025-05-05 11:06:58 +0300

    'Week7Assignment'

--- answers.sql
+++ answers.sql
@@ -1,27 +1,42 @@
 -- Answer Question 1
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+-- Answer Question 2
 CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
-CREATE TABLE OrderProducts (
-    OrderProductID INT AUTO_INCREMENT PRIMARY KEY,
+
+CREATE TABLE Product (
     OrderID INT,
-    Product VARCHAR(100),
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
-);
-
--- Answer Question 2
-CREATE TABLE Customer (
-    CustomerID INT PRIMARY KEY,
-    CustomerName VARCHAR(100)
-);
-
-CREATE TABLE Orders (
-    OrderID INT,
-    CustomerID INT,
     Product VARCHAR(100),
     Quantity INT,
     PRIMARY KEY (OrderID, Product),
-    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
